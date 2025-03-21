3b137e24bbd41be1bf30c0b47b4683948f307e1c
eeghor <eeghor@gmail.com>
2025-03-21 19:28:33 +0700

    looks alright

--- L2.py
+++ L2.py
@@ -10,9 +10,13 @@ from llama_index.core.workflow import (
     step,
     Context,
 )
+from llama_index.llms.openai import OpenAI
+from llama_index.llms.anthropic import Anthropic
 from llama_parse import LlamaParse
 import os
-from llama_index.llms.anthropic import Anthropic
+from llama_index.core import StorageContext, load_index_from_storage
+from llama_index.core.tools import FunctionTool
+from llama_index.core.agent import FunctionCallingAgent
 from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.core import VectorStoreIndex
 
@@ -21,9 +25,15 @@ from dotenv import load_dotenv
 load_dotenv()
 nest_asyncio.apply()
 
-api_key = os.getenv("ANTHROPIC_API_KEY")
-llama_cloud_api_key = os.getenv("LLAMA_CLOUD_API_KEY")
+api_key = {
+    "anthropic": os.getenv("ANTHROPIC_API_KEY"),
+    "openai": os.getenv("OPENAI_API_KEY"),
+    "llama_cloud": os.getenv("LLAMA_CLOUD_API_KEY"),
+}
 
+print(f"OpenAI API Key loaded: {'Yes' if api_key['openai'] else 'No'}")
+print(f"Anthropic API Key loaded: {'Yes' if api_key['anthropic'] else 'No'}")
+print(f"LlamaCloud API Key loaded: {'Yes' if api_key['llama_cloud'] else 'No'}")
 
 # Events are simple data classes that pass information
 # between workflow steps
@@ -111,7 +121,7 @@ class MyWorkflow(Workflow):
 
     @step
     async def step_two(self, ctx: Context, ev: FirstEvent) -> SecondEvent:
-        llm = Anthropic(model="claude-3-7-sonnet-latest", api_key=api_key)
+        llm = Anthropic(model="claude-3-7-sonnet-latest", api_key=api_key["anthropic"])
         generator = await llm.astream_complete(
             "Please give me the first 50 words of Moby Dick, a book in the public domain."
         )
@@ -243,7 +253,7 @@ if __name__ == "__main__":
     asyncio.run(main())
 
     documents = LlamaParse(
-        api_key=llama_cloud_api_key,
+        api_key=api_key["llama_cloud"],
         base_url=os.getenv("LLAMA_CLOUD_BASE_URL"),
         result_type="markdown",
         content_guideline_instruction="This is a resume, gather related facts together and format it as bullet points with headers",
@@ -253,20 +263,116 @@ if __name__ == "__main__":
 
     print(documents[2].text)
 
-    from llama_index.embeddings.openai import OpenAIEmbedding
-    from llama_index.core import VectorStoreIndex
-
     index = VectorStoreIndex.from_documents(
         documents,
         embed_model=OpenAIEmbedding(
-            model_name="text-embedding-3-small", api_key=os.getenv("OPEN_AI_API_KEY")
+            model_name="text-embedding-3-small", api_key=api_key["openai"]
         ),
     )
+    print("Index created")
+
     query_engine = index.as_query_engine(
-        llm=Anthropic(model="claude-3-7-sonnet-latest", api_key=api_key),
+        llm=Anthropic(model="claude-3-7-sonnet-latest", api_key=api_key["anthropic"]),
         similarity_top_k=5,
     )
+
+    print("Query engine created")
+
     response = query_engine.query(
         "What is this person's name and what was their most recent job?"
     )
     print(response)
+
+    # store index to disk
+    storage_dir = "./storage"
+
+    index.storage_context.persist(persist_dir=storage_dir)
+    print("index made persistent")
+
+    # Check if the index is stored on disk
+    if os.path.exists(storage_dir):
+        # Load the index from disk
+        storage_context = StorageContext.from_defaults(persist_dir=storage_dir)
+        restored_index = load_index_from_storage(storage_context)
+        print("Index restored from disk.")
+    else:
+        print("Index not found on disk.")
+
+    print("Querying the restored index")
+    response = restored_index.as_query_engine().query(
+        "What is this person's name and what was their most recent job?"
+    )
+    print(response)
+
+    def query_resume(q: str) -> str:
+        """Answers questions about a specific resume."""
+        # we're using the query engine we already created above
+        response = query_engine.query(
+            f"This is a question about the specific resume we have in our database: {q}"
+        )
+        return response.response
+
+    resume_tool = FunctionTool.from_defaults(fn=query_resume)
+
+    agent = FunctionCallingAgent.from_tools(
+        tools=[resume_tool],
+        llm=Anthropic(model="claude-3-7-sonnet-latest", api_key=api_key["anthropic"]),
+        verbose=True,
+    )
+    print("Agent set up. Asking a question about the resume")
+
+    response = agent.chat("How many years of experience does the applicant have?")
+    print(response)
+
+    class QueryEvent(Event):
+        query: str
+
+    class RAGWorkflow(Workflow):
+        storage_dir = "./storage"
+        llm: OpenAI
+        query_engine: VectorStoreIndex
+
+        # the first step will be setup
+        @step
+        async def set_up(self, ctx: Context, ev: StartEvent) -> QueryEvent:
+            if not ev.resume_file:
+                raise ValueError("No resume file provided")
+
+            # define an LLM to work with
+            self.llm = OpenAI(model="gpt-4o-mini", api_key=api_key["openai"])
+
+            # ingest the data and set up the query engine
+            if os.path.exists(self.storage_dir):
+                # you've already ingested your documents
+                storage_context = StorageContext.from_defaults(
+                    persist_dir=self.storage_dir
+                )
+                index = load_index_from_storage(storage_context)
+            else:
+                # parse and load your documents
+                documents = LlamaParse(
+                    result_type="markdown",
+                    content_guideline_instruction="This is a resume, gather related facts together and format it as bullet points with headers",
+                ).load_data(ev.resume_file)
+                # embed and index the documents
+                index = VectorStoreIndex.from_documents(
+                    documents,
+                    embed_model=OpenAIEmbedding(
+                        model_name="text-embedding-3-small", api_key=api_key["openai"]
+                    ),
+                )
+                index.storage_context.persist(persist_dir=self.storage_dir)
+
+            # either way, create a query engine
+            self.query_engine = index.as_query_engine(llm=self.llm, similarity_top_k=5)
+
+            # now fire off a query event to trigger the next step
+            return QueryEvent(query=ev.query)
+
+        # the second step will be to ask a question and return a result immediately
+        @step
+        async def ask_question(self, ctx: Context, ev: QueryEvent) -> StopEvent:
+            response = self.query_engine.query(
+                f"This is a question about the specific resume we have in our database: {ev.query}"
+            )
+            return StopEvent(result=response.response)
